d49f82aea5fde4c7511f3645f3a598bc28683a3e
Oliver Probst <pro@kswe.ch>
2024-08-08 20:45:27 +0200

    feat: initial version

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Imperatives Programmieren | iprog.ginf.ch</title>
+    <?php
+    include("head.php");
+    ?>
+</head>
+<body>
+<header>
+    <?php
+    include("header.html");
+    ?>
+</header>
+<main>
+    <section class="section is-medium">
+        <div id="tools" style="visibility:hidden; position: relative; top: -25px;">Tools-Anchor</div>
+        <h1 class="title">Tools</h1>
+        <?php
+        include("tools.html");
+        ?>
+    </section>
+</main>
+<footer class="footer">
+    <?php
+    include("footer.php");
+    ?>
+</footer>
+</body>
+</html>
